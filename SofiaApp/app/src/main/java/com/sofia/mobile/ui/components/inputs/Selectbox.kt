2d50e207438853664261b57816b4f2f1bdbc7ef1
aasjunior <adeldivo@hotmail.com>
2023-11-06 00:11:00 -0300

    feat: Selectbox

--- SofiaApp/app/src/main/java/com/sofia/mobile/ui/components/inputs/Selectbox.kt
+++ SofiaApp/app/src/main/java/com/sofia/mobile/ui/components/inputs/Selectbox.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,26 +22,41 @@ import androidx.compose.ui.unit.dp
 import com.sofia.mobile.ui.components.text.body1
 import com.sofia.mobile.ui.components.text.body2
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Selectbox(label: String, items: List<String>) {
-    var selectedItems by remember { mutableStateOf(List(items.size) { false }) }
+fun Selectbox(label: String, options: List<String>) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
 
-    Column {
-        Text(text = label, style = body2)
-        items.forEachIndexed { index, item ->
-            Row(Modifier.padding(vertical = 8.dp)) {
-                Checkbox(
-                    checked = selectedItems[index],
-                    onCheckedChange = { isChecked ->
-                        selectedItems = selectedItems.mapIndexed { i, isSelected ->
-                            if (i == index) isChecked else isSelected
-                        }
-                    }
-                )
-                Text(
-                    text = item,
-                    style = body1,
-                    modifier = Modifier.padding(start = 8.dp)
+    ExposedDropdownMenuBox(
+        modifier = Modifier,
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+    ) {
+        TextField(
+            modifier = Modifier.menuAnchor(),
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = {},
+            label = { Text(label) },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+
+        ExposedDropdownMenu(
+            modifier = Modifier,
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            options.forEach { selectionOption ->
+                DropdownMenuItem(
+                    modifier = Modifier,
+                    text = { Text(selectionOption) },
+                    onClick = {
+                        selectedOptionText = selectionOption
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
                 )
             }
         }
@@ -47,5 +67,5 @@ fun Selectbox(label: String, items: List<String>) {
 @Composable
 fun SelectboxPreview(){
     val ethnicities = listOf("Branca", "Parda", "Preta", "Amarela", "Indígena")
-    Selectbox(label = "Etnia", items = ethnicities)
+    Selectbox(label = "Etnia", options = ethnicities)
 }
