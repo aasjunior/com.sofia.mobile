5b2d8d21b6aaa3a3c6ad038e012122cd2e661e5d
aasjunior <adeldivo@hotmail.com>
2023-11-18 21:04:18 -0300

    feat(ui): patient profile card

--- SofiaApp/app/src/main/java/com/sofia/mobile/ui/screens/PatientProfileScreen.kt
+++ SofiaApp/app/src/main/java/com/sofia/mobile/ui/screens/PatientProfileScreen.kt
@@ -1,11 +1,23 @@
 package com.sofia.mobile.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.sofia.mobile.repository.RepositoryProvider
+import com.sofia.mobile.ui.components.cards.PatientProfileCard
+import com.sofia.mobile.ui.components.navbar.appbar.CustomTopAppBar
 import com.sofia.mobile.ui.viewmodels.GenericViewModelFactory
 import com.sofia.mobile.ui.viewmodels.PatientProfileViewModel
 
@@ -25,11 +37,26 @@ fun PatientProfileScreen(
     }
 
     // Observe o estado do paciente e exiba os detalhes do paciente
-    val patient = viewModel.patient.value
+    val paciente = viewModel.patient.value
     val errorMessage = viewModel.errorMessage.value
 
-    if(patient != null){
-        Text(text = "${patient.nome}")
+    if(paciente != null){
+        Scaffold(
+            topBar = {
+                CustomTopAppBar()
+            }
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(modifier = Modifier.height(12.dp))
+                PatientProfileCard(paciente)
+            }
+        }
     } else if(errorMessage != null){
         Text(text = "deu erro")
     } else {
