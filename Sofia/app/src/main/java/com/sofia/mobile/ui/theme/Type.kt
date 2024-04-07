c3af50c07e99a150c0fb92932f81068bbf0dcc93
aasjunior <adeldivo@hotmail.com>
2024-04-07 15:44:04 -0300

    feat: add sofiatheme

--- Sofia/app/src/main/java/com/sofia/mobile/ui/theme/Type.kt
+++ Sofia/app/src/main/java/com/sofia/mobile/ui/theme/Type.kt
@@ -5,6 +5,46 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sofia.mobile.ui.theme.fonts.PoppinsFontFamily
+
+private val poppins = PoppinsFontFamily()
+
+object SofiaTypography{
+    val title20 = TextStyle(
+        fontFamily = poppins.medium,
+        fontSize = 20.sp
+    )
+
+    val title24 = TextStyle(
+        fontFamily = poppins.semibold,
+        fontSize = 24.sp
+    )
+
+    val title28 = TextStyle(
+        fontFamily = poppins.semibold,
+        fontSize = 28.sp
+    )
+
+    val text1 = TextStyle(
+        fontFamily = poppins.regular,
+        fontSize = 14.sp
+    )
+
+    val text2 = TextStyle(
+        fontFamily = poppins.semibold,
+        fontSize = 14.sp
+    )
+
+    val text12 = TextStyle(
+        fontFamily = poppins.regular,
+        fontSize = 12.sp
+    )
+
+    val text16 = TextStyle(
+        fontFamily = poppins.medium,
+        fontSize = 16.sp
+    )
+}
 
 // Set of Material typography styles to start with
 val Typography = Typography(
--- docs/README.md
+++ docs/README.md
@@ -300,3 +300,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.0-alpha05")
 }
 ```
+
+# NavGraph
+# Autenticação
